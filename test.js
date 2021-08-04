7f28f29a2c273cfb725bf855b35be6011dd302c5
Jay <63338983+NirmithVictor@users.noreply.github.com>
2021-08-04 08:52:13 +0300

    Add files via upload

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 console.log("IT IS WORKING NIRMITH")
 
-/*fetch('https://gh-pinned-repos-5l2i19um3.vercel.app/?username=NirmithVictor').then(res => res.json()).then((content)=>{
+fetch('https://gh-pinned-repos-5l2i19um3.vercel.app/?username=NirmithVictor').then(res => res.json()).then((content)=>{
     console.log(content)
-})*/
+})
