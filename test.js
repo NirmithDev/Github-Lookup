7841cb9590d99151d62bf4c8a042f12686aaf254
Jay <63338983+NirmithVictor@users.noreply.github.com>
2021-08-09 19:40:22 +0300

    Add files via upload

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
 console.log("IT IS WORKING NIRMITH")
-
-fetch('https://gh-pinned-repos-5l2i19um3.vercel.app/?username=NirmithVictor').then(res => res.json()).then((content)=>{
-    console.log(content)
-    fetch('https://api.github.com/users/NirmithVictor/followers').then(res => res.json()).then((content)=>{
-    console.log(content)
+//yeah this might be a bad practice
+//but if it works it works
+fetch('https://gh-pinned-repos-5l2i19um3.vercel.app/?username=NirmithVictor').then(res => res.json()).then((content2)=>{
+    //console.log(content)
+    fetch('https://api.github.com/users/NirmithVictor/followers').then(res => res.json()).then((content1)=>{
+    //console.log(content)
+        fetch('https://api.github.com/users/NirmithVictor/following').then(res => res.json()).then((content)=>{
+        console.log(content)
+        console.log(content1)
+        console.log(content2)
+        })
     })
 })
