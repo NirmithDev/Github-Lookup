5c49294acf40aef45544f05c44534ce2e757d0dc
Jay <63338983+NirmithVictor@users.noreply.github.com>
2021-08-09 19:31:15 +0300

    Add files via upload

--- test.js
+++ test.js
@@ -2,4 +2,7 @@ console.log("IT IS WORKING NIRMITH")
 
 fetch('https://gh-pinned-repos-5l2i19um3.vercel.app/?username=NirmithVictor').then(res => res.json()).then((content)=>{
     console.log(content)
+    fetch('https://api.github.com/users/NirmithVictor/followers').then(res => res.json()).then((content)=>{
+    console.log(content)
+    })
 })
